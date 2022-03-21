54694d2dcb52d78355c3faa72093b2193e181d1b
ItzAnsh <anshbhasin3723@gmail.com>
2022-03-21 12:04:21 +0530

    c

---
+++ js/1.js
@@ -0,0 +1,16 @@
+var a = document.getElementById("a");
+
+function text1() {
+	a.innerHTML =
+		"Personality is what makes a person a unique person, and it is recognizable soon after birth. A child's personality has several components: temperament, environment, and character. Temperament is the set of genetically determined traits that determine the child's approach to the world and how the child learns about the world. There are no genes that specify personality traits, but some genes do control the development of the nervous system, which in turn controls behavior.";
+}
+
+function text2() {
+	a.innerHTML =
+		"Being able to communicate effectively is perhaps the most important of all life skills. It is what enables us to pass information to other people, and to understand what is said to us. You only have to watch a baby listening intently to its mother and trying to repeat the sounds that she makes to understand how fundamental is the urge to communicate. ";
+}
+
+function text3() {
+	a.innerHTML =
+		"It is important to look after your body, to help you to stay well and healthy for as long as you can. However, advice about caring for your body is conflicting and often confusing. It can be hard to know what’s best. Many people abandon any effort to take care of themselves because it’s just too difficult.";
+}
