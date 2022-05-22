5b9a8a865226c92a9e610686f51ca3b8a007f31f
ItzAnsh <anshbhasin3723@gmail.com>
2022-05-22 18:02:28 +0530

    psuh

---
+++ js/1.js
@@ -0,0 +1,11 @@
+function text1(){
+  document.getElementById("a").innerHTML = "Personality Developement is a skill everyone wants to acquire these days ,being a great personality attracts every human being towards you and gives you way more than just a spotlight in the crowd.";
+}
+
+function text2(){
+  document.getElementById("a").innerHTML = "Communication Skills is another one of the most aspect of life, which can attract people by just a few sentences."
+}
+
+function text3(){
+  document.getElementById("a").innerHTML = "Physical growth is one of the most common words you can hear in public these days. People use gyms, etc."
+}
